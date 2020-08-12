3d3e761bcccc097a2ff8f04db2fb9a679fd98c55
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-08-12 20:26:02 +0200

    Updates

---
+++ precache-manifest.6ac7b2bc13153b7a16d9a92a2091ac52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d289cda3397cacb195a0258abdb60899",
+    "url": "/currency-convert-react/index.html"
+  },
+  {
+    "revision": "8a8c7ba02e10d3f51ff2",
+    "url": "/currency-convert-react/static/css/main.7644871a.chunk.css"
+  },
+  {
+    "revision": "7bb7e0b856817d24b8ee",
+    "url": "/currency-convert-react/static/js/2.cf2817d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency-convert-react/static/js/2.cf2817d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8c7ba02e10d3f51ff2",
+    "url": "/currency-convert-react/static/js/main.433348a3.chunk.js"
+  },
+  {
+    "revision": "c2a1d8afe73580a42677",
+    "url": "/currency-convert-react/static/js/runtime-main.9529d0fd.js"
+  }
+]);
